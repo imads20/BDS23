70025860f51025ab81ee705243b1ebbbfa60c4b4
Isabella Madsen <101288790+imads20@users.noreply.github.com>
2023-11-05 14:19:43 +0100

    update

--- M2_Final_Assignment/Network_Analysis/app.py
+++ M2_Final_Assignment/Network_Analysis/app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pickle
 import requests
+import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Remove warnings
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def get_pickle(filename, path):
     # Function for downloading pickle files
@@ -17,6 +23,26 @@ def get_pickle(filename, path):
         print('Failed to retrieve the pickle file.')
     
     return file
+
+# Function for loading components
+@st.cache_data  # Cache the function to enhance performance
+def load_components():
+
+    # Define path of components
+    path = 'https://raw.githubusercontent.com/imads20/BDS23/main/M2_Final_Assignment/Network_Analysis/components/'
+
+    # For pickle files we first need to download the files onto the server as pickle is only designed to get files locally. 
+    # To do this we have made a function, which we can apply
+    G = get_pickle("G.pkl", path)
+    cent_degree = get_pickle("cent_degree.pkl", path)
+    cent_between = get_pickle("cent_between.pkl", path)
+    cent_eigen = get_pickle("cent_eigen.pkl", path)
+
+    df_top = pd.read_json(path + 'df_top.json')
+
+    return G, cent_degree, cent_between, cent_eigen, df_top
+
+G, cent_degree, cent_between, cent_eigen, df_top = load_components()
 
 st.title("High-Energy Physics Citation Network")
 
@@ -50,4 +76,38 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+######## One-Mode Visualization ########
 st.write("## One-Mode Visualization")
+
+fig = nx.draw(G, with_labels=False, node_size=25)
+st.pyplot(fig)
+
+
+# Horizontal line
+st.markdown(
+    '<hr style="border: none; height: 5px; background: linear-gradient(90deg, #FFA500, #000000);">',
+    unsafe_allow_html=True
+)
+
+######## Centrality Visualizations ########
+st.write("## Centrality Visualizations")
+
+central_select = st.selectbox(label = "Centrality Measure",
+                              options=['Degree Centrality', 'Betweenness Centrality', 'Eigenvector Centrality'])
+
+G_layout = nx.layout.fruchterman_reingold_layout(G)
+
+if central_select == 'Degree Centrality': 
+    node_sizes = [2.5*cent_degree[node] for node in G.nodes()]
+    fig1 = nx.draw(G, with_labels=False, node_size=node_sizes)
+    st.pyplot(fig1)
+elif central_select == 'Betweenness Centrality': 
+    node_sizes = [2.5*cent_between[node] for node in G.nodes()]
+    fig2 = nx.draw(G, with_labels=False, node_size=node_sizes)
+    st.pyplot(fig2)
+elif central_select == 'Eigenvector Centrality': 
+    node_sizes = [4.5*cent_eigen[node] for node in G.nodes()]
+    fig2 = nx.draw(G, with_labels=False, node_size=node_sizes)
+    st.pyplot(fig2)
+
+st.dataframe(df_top)
